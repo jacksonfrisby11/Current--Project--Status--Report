ed204c7605696f1442a30b806549aae8c4f61b96
jacksonfrisby11 <jacksonfrisby@gmail.com>
2025-10-07 14:37:01 -0400

    Project Report Phase 3 Update

--- Code/Data_Comparison.py
+++ Code/Data_Comparison.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.subplots as sp
+import plotly.graph_objects as go
 import plotly.io as pio
 pio.renderers.default = 'browser'
 import warnings 
 warnings.filterwarnings('ignore')
 
-df = pd.read_csv(r"C:\Users\Jacks\OneDrive\Desktop\Senior Project\StudentPerformanceFactors.csv")
+df = pd.read_csv(r"C:\Users\Jacks\OneDrive\Desktop\Senior Project\StudentPerformanceFactors (1).csv")
 
-# Region 1
+
+# Region 1 (9/29/25)
 ## Create a scatter plot to visualize the relationship between study hours vs. learning disabilities, and the resulting exam scores
 fig = px.scatter(df, 
                  x="Hours_Studied", 
@@ -26,7 +29,7 @@ atg = df.groupby("Learning_Disabilities")["Exam_Score"].mean()
 # End Region 1
 
 
-# Region 2
+# Region 2 (10/1/25)
 ## Create a box plot to show the distribution of exam scores based on parental education level and learning disabilities
 fig = px.box(df, 
              x="Parental_Education_Level", 
@@ -48,3 +51,36 @@ fig.update_layout(width=700, height=600,template='plotly_white',title_x=0.5)
 # Display box plot
 fig.show()
 # End Region 2
+
+#Region 3 (10/7/25)
+## This section visualizes the distribution of all categorical columns in the dataset
+cat_cols = df.select_dtypes(include='O')
+cols = 2
+rows = (len(cat_cols.columns) + 1) // cols 
+
+colors = ['#404546', '#4C6B73', '#7492C1', '#8A7D4C', '#A7B7A7', '#A69585', '#6D7F8C', 
+          '#4F7D9E', '#82A1B2', '#A19B8F', '#B59A6A', '#9A7A48', '#706E4C']
+
+# Create a subplot figure with titles for each categorical feature
+fig = sp.make_subplots(rows=rows, cols=cols, subplot_titles=[col for col in cat_cols.columns])
+# Loop through each categorical column to create a bar chart
+for i, column in enumerate(cat_cols.columns):
+    row = (i // cols) + 1  
+    col = (i % cols) + 1   
+    # Count occurrences of each unique category in the column
+    count_df = df[column].value_counts().reset_index()
+    count_df.columns = [column, 'count']
+    
+    fig.add_trace(
+        go.Bar(x=count_df[column], y=count_df['count'], name=column, marker_color=colors[i],text=count_df['count'],textposition='auto'),
+        row=row, col=col
+    )
+# Update layout settings
+fig.update_layout(
+    title_text="Distribution of Each Categorical Features",
+    height=2500, width=800,  
+    showlegend=True,
+    template='plotly_white'
+)
+# Display bar charts
+fig.show()
